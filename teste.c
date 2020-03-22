069a1c445a5352cbc41251a0a2580da85d0ffc90
Gustavo <gustavo.denardin@gmail.com>
2020-03-22 11:27:01 -0300

    Versão de desenvolvimento 2.0

--- teste.c
+++ teste.c
@@ -3,6 +3,6 @@
 int main(void){
 	printf("Olá mundo v1.0\n\r");
 	printf("Teste de novo commit!\n\r");
-	printf("Versão de desenvolvimento.\n\r");
+	printf("Versão de desenvolvimento 2.0.\n\r");
 	return 0;
 }
